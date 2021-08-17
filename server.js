22f269ad58fc4bae9254d2dfb3f673e15b702ee5
mathew-harvey <mathewharvey@gmail.com>
2021-08-17 13:14:11 +0800

    Finished Video

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const server = require ('http').Server(app)
-const io = require ('socket.io')(server)
-const { v4: uuidV4 } = require ('uuid')
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
 
 
 
@@ -15,14 +15,18 @@ app.get('/', (req, res) => {
 })
 
 app.get('/:room', (req, res) => {
-    res.render('room', {roomId: req.params.room})
+    res.render('room', { roomId: req.params.room })
 })
 
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
-socket.join(roomId)
-socket.broadcast.to(roomId).emit('user-connected', userId)
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit('user-connected', userId)
+
+        socket.on('disconnect', () => {
+            socket.broadcast.to(roomId).emit('user-disconnected', userId)
+        })
     })
 })
- 
+
 server.listen(3000)
